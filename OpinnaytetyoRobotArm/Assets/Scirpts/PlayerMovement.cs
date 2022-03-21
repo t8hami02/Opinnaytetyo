01bd2ada0b08cc9fb8f137d1ea1213d0a63f01d2
t8hami02 <t8hami02@students.oamk.fi>
2022-03-21 14:40:19 +0200

    SteamVR laserpointer progress

--- OpinnaytetyoRobotArm/Assets/Scirpts/PlayerMovement.cs
+++ OpinnaytetyoRobotArm/Assets/Scirpts/PlayerMovement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Not used with VR player
+
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
